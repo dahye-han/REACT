771f6de581f796763fbba375c4bf85bac154032b
hanae <doithanae@gmail.com>
2020-10-22 21:37:46 +0900

    컴포넌트 재사용

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 
 let h1 = React.createElement('h1', null, 'Hello World!')
 
+class HelloWorld extends React.Component {
+  render () {
+    return React.createElement('div', null, h1, h1)
+  }
+}
+
 ReactDOM.render(
-  React.createElement('div', null, h1, h1),
+  React.createElement('div', null, React.createElement(HelloWorld), React.createElement(HelloWorld), React.createElement(HelloWorld)),
   document.getElementById('root')
 );
   
