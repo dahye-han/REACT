72cc892dea1ea11b18058c0b3f5a2ce8b9a17ccc
hanae <doithanae@gmail.com>
2020-10-22 21:48:34 +0900

    props 사용하기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let h1 = React.createElement('h1', null, 'Hello World!')
-
 class HelloWorld extends React.Component {
   render () {
-    if(this.props.heading) return <h1>hello</h1>
-    else return <p>Hello</p> 
+    return React.createElement(
+      'h1', this.props, 'Hello ' + this.props.frameworkName + ' world!!'
+    ) 
   }
 }
 
 ReactDOM.render(
-  React.createElement('div', null, React.createElement(HelloWorld), React.createElement(HelloWorld), React.createElement(HelloWorld)),
+  React.createElement(
+    'div', 
+    null, 
+    React.createElement(HelloWorld, {
+      if: 'ember',
+      frameworkName: 'Ember.js',
+      title: 'A framework for creating ambitious web applications.'}),
+    React.createElement(HelloWorld, {
+      if: 'backbone',
+      frameworkName: 'Backbone.js',
+      title: 'Backbone.js gives structure to web applications...'}),
+    React.createElement(HelloWorld, {
+      if: 'angular',
+      frameworkName: 'Angular.js',
+      title: 'Superheroic JavaScript MVW Framework'})
+      ),
   document.getElementById('root')
 );
   
