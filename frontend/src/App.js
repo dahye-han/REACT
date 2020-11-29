1fb448a7d7e8162ca1615da6815fa91ad8eed449
hanae <doithanae@gmail.com>
2020-11-29 21:06:44 +0900

    은구에게 속지마

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,112 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
-function App() {
-  const [message, setMessage] = useState("");
+class App extends Component {
 
-  useEffect(() =>{
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  },[])
+  id = 3
+  
+  baseState = {
+    input: '',
+    todos: [
+      { id: 0, text: '아침밥', checked: false },
+      { id: 1, text: '점심밥', checked: false },
+      { id: 2, text: '저녁밥', checked: false }
+    ]
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{message}</h1>
-      </header>
-      <p className="App-intro">
-        To get startedm edut <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: '아침밥', checked: false },
+      { id: 1, text: '점심밥', checked: false },
+      { id: 2, text: '저녁밥', checked: false }
+    ]
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value // input 의 다음 바뀔 값
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '', // 인풋 비우고
+      // concat 을 사용하여 배열에 추가
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 면 handleCreate 호출
+    if(e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
+
+    const nextTodos = [...todos]; // 배열을 복사
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = { 
+      ...selected, 
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  handleReset = () => {
+    this.setState(this.baseState);
+  }
+
+  render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove,
+      handleReset
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form 
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+          onReset={handleReset}
+        />
+        )}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      </TodoListTemplate>
+    )
+  }
 }
 
 export default App;
