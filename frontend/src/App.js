c128280d5f43569746abe3bc88e5611a0aa12cb9
hanae <doithanae@gmail.com>
2020-12-06 20:09:49 +0900

    은구에게 속지마 업데이트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,112 +1,31 @@
-import React, { Component } from 'react';
-import TodoListTemplate from './components/TodoListTemplate';
-import Form from './components/Form';
-import TodoItemList from './components/TodoItemList';
+import React from 'react';
+import  { createGlobalStyle } from 'styled-components';
+import MealTemplate from './components/MealTemplate';
+import MealHead from './components/MealHead';
+import MealList from './components/MealList';
+import MealCreate from './components/MealCreate';
+import { MealProvider } from './components/MealContext';
 
-class App extends Component {
-
-  id = 3
-  
-  baseState = {
-    input: '',
-    todos: [
-      { id: 0, text: '아침밥', checked: false },
-      { id: 1, text: '점심밥', checked: false },
-      { id: 2, text: '저녁밥', checked: false }
-    ]
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
   }
+`
+;
 
-  state = {
-    input: '',
-    todos: [
-      { id: 0, text: '아침밥', checked: false },
-      { id: 1, text: '점심밥', checked: false },
-      { id: 2, text: '저녁밥', checked: false }
-    ]
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      input: e.target.value // input 의 다음 바뀔 값
-    });
-  }
-
-  handleCreate = () => {
-    const { input, todos } = this.state;
-    this.setState({
-      input: '', // 인풋 비우고
-      // concat 을 사용하여 배열에 추가
-      todos: todos.concat({
-        id: this.id++,
-        text: input,
-        checked: false
-      })
-    });
-  }
-
-  handleKeyPress = (e) => {
-    // 눌려진 키가 Enter 면 handleCreate 호출
-    if(e.key === 'Enter') {
-      this.handleCreate();
-    }
-  }
-
-  handleToggle = (id) => {
-    const { todos } = this.state;
-
-    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
-    const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index]; // 선택한 객체
-
-    const nextTodos = [...todos]; // 배열을 복사
-
-    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
-    nextTodos[index] = { 
-      ...selected, 
-      checked: !selected.checked
-    };
-
-    this.setState({
-      todos: nextTodos
-    });
-  }
-
-  handleRemove = (id) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.filter(todo => todo.id !== id)
-    });
-  }
-
-  handleReset = () => {
-    this.setState(this.baseState);
-  }
-
-  render() {
-    const { input, todos } = this.state;
-    const {
-      handleChange,
-      handleCreate,
-      handleKeyPress,
-      handleToggle,
-      handleRemove,
-      handleReset
-    } = this;
-
-    return (
-      <TodoListTemplate form={(
-        <Form 
-          value={input}
-          onKeyPress={handleKeyPress}
-          onChange={handleChange}
-          onCreate={handleCreate}
-          onReset={handleReset}
-        />
-        )}>
-        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
-      </TodoListTemplate>
-    )
-  }
+function App() {
+  return (
+    <>
+      <MealProvider>
+        <GlobalStyle />
+        <MealTemplate>
+          <MealHead />
+          <MealList />
+          <MealCreate />
+        </MealTemplate>
+      </MealProvider>
+    </>
+  );
 }
 
 export default App;
